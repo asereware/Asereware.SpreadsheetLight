5a464b11e563e5c7a17f0ea8fc97aff37ea0bdd3
Asereware <asereware@yahoo.com>
2020-08-31 12:31:44 -0500

    Add project files.

---
+++ Asereware.SpreadsheetLight.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpreadsheetLight.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpreadsheetLight.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("240abe1b-58af-480e-9b97-8e0334962ef7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Asereware.SpreadsheetLight.Tests/SLDocumentTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetLight.Tests
+{
+    [TestClass()]
+    public class SLDocumentTests
+    {
+        [TestMethod()]
+        public void SetCellValueTest()
+        {
+
+            using (var sd = new SLDocument())
+            {
+                sd.SetCellValue("A1", "Mi valor de texto");
+                sd.SetCellValue("B1", true);
+                sd.SetCellValue(1, 3, DateTime.Now);
+                sd.SaveAs("Hello World.xlsx");
+            }
+        }
+    }
+}
