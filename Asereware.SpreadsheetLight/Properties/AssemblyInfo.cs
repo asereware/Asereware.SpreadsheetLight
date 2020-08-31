9fe54c6029600db09598ecfdbf48511c39a84bab
Asereware <asereware@yahoo.com>
2020-08-31 18:32:17 -0500

    - Asereware signature in code.

--- Asereware.SpreadsheetLight/Properties/AssemblyInfo.cs
+++ Asereware.SpreadsheetLight/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpreadsheetLight by Asereware")]
-[assembly: AssemblyDescription("Compatible with Open XML 3.11")]
+[assembly: AssemblyDescription("SpreadsheetLight compatible with Open XML 3.11")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Asereware")]
 [assembly: AssemblyProduct("SpreadsheetLight")]
