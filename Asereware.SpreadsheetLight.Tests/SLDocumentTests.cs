e6cf2f6b4caa0cc4b83577faf42cd3c832a61306
Asereware <asereware@yahoo.com>
2020-09-02 11:46:02 -0500

    - New SpreadsheetLight.Extensions with SLDocument extensions to create System.Data.DataTable and System.Data.DataView.

--- Asereware.SpreadsheetLight.Tests/SLDocumentTests.cs
+++ Asereware.SpreadsheetLight.Tests/SLDocumentTests.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpreadsheetLight;
+using SpreadsheetLight.Extensions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +14,55 @@ namespace SpreadsheetLight.Tests
     [TestClass()]
     public class SLDocumentTests
     {
+        private const string _coFileName = "Hello World.xlsx";
+
         [TestMethod()]
         public void SetCellValueTest()
         {
-
             using (var sd = new SLDocument())
             {
-                sd.SetCellValue("A1", "Mi valor de texto");
+                sd.SetCellValue("A1", "My text value");
                 sd.SetCellValue("B1", true);
-                sd.SetCellValue(1, 3, DateTime.Now);
-                sd.SaveAs("Hello World.xlsx");
+                sd.SetCellValue(1, 3, DateTime.Now.ToString("s"));
+                sd.SaveAs(_coFileName);
             }
+
+            Assert.IsTrue(File.Exists(_coFileName));
+            CreateDataTableExtension();
+            CreateDataViewExtension();
+        }
+
+        private void CreateDataTableExtension()
+        {
+            System.Data.DataTable dt = null;
+            using (var sd = new SLDocument(_coFileName))
+            {
+                dt = sd.CreateDataTable(hasHeaders: false);                
+            }
+
+            Assert.IsTrue(dt.Columns.Count == 3);
+            Assert.IsTrue(dt.Rows.Count == 1);
+        }
+
+        private void CreateDataViewExtension()
+        {
+            System.Data.DataView dv = null;
+            using (var sd = new SLDocument(_coFileName))
+            {
+                dv = sd.CreateDataView(hasHeaders: false);
+            }
+
+            Assert.IsTrue(dv.Count == 1);
+            Assert.IsTrue(dv.Table.Columns.Count == 3);
+            Assert.IsTrue(dv.Table.Rows.Count == 1);
+
+            var col1Name = dv.Table.Columns[0].ColumnName;
+            dv.RowFilter = $"{col1Name} LIKE '%text%'";
+            Assert.IsTrue(dv.Count == 1);
+            dv.RowFilter = $"{col1Name} LIKE '%none%'";
+            Assert.IsTrue(dv.Count == 0);
+            dv.RowFilter = String.Empty;
+            Assert.IsTrue(dv.Count == 1);
         }
     }
 }
---
+++ Asereware.SpreadsheetLight/Extensions/SLDocumentExtensions.cs
@@ -0,0 +1,90 @@
+﻿// ***********************************************************************
+// Assembly         : Metis.Applications.Import
+// Author           : asereware
+// Created          : 10-10-2018
+//
+// Last Modified By : asereware
+// Last Modified On : 10-10-2018
+// ***********************************************************************
+// <copyright file="SLDocumentExtensions.cs" company="Asereware">
+//     Copyright © Asereware 2014
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using SpreadsheetLight;
+using System;
+using System.Data;
+
+namespace SpreadsheetLight.Extensions
+{
+    /// <summary>
+    /// Class SpreadSheetLightExtensions.
+    /// </summary>
+    public static class SLDocumentExtensions
+    {
+        /// <summary>
+        /// Creates the data table.
+        /// </summary>
+        /// <param name="sheet">The sheet.</param>
+        /// <param name="hasHeaders">if set to <c>true</c> [has headers].</param>
+        /// <returns>DataTable.</returns>
+        public static DataTable CreateDataTable(this SLDocument sheet, bool hasHeaders = true)
+        {
+            DataTable dt = new DataTable(sheet.GetCurrentWorksheetName());
+            var stats = sheet.GetWorksheetStatistics();
+            var columnsCount = stats.NumberOfColumns;
+            var rowsCount = stats.NumberOfRows;
+            string colName;
+            for (int i = 0; i < columnsCount; i++)
+            {
+                colName = null;
+                var index = i + 1;
+                if (hasHeaders)
+                {
+                    colName = sheet.GetCellValueAsString(1, i + 1);
+                    if (!String.IsNullOrWhiteSpace(colName) && dt.Columns.Contains(colName))
+                    {
+                        //Set unknown alias due to the column will be ignored.
+                        //This solution avoids alternative that is to count the similar names to increment a counter.
+                        colName = String.Concat(colName, "-", Guid.NewGuid().ToString("N"));
+                    }
+                }
+
+                if(String.IsNullOrWhiteSpace(colName))                
+                {
+                    colName = $"Col{index.ToString().PadLeft(4, '0')}";
+                }
+
+                dt.Columns.Add(new DataColumn(colName));
+            }
+
+            for (int i = 1; i <= rowsCount; i++)
+            {
+                if (hasHeaders && i == 1)
+                    continue;
+
+                DataRow dr = dt.NewRow();
+                for (int j = 1; j <= columnsCount; j++)
+                {
+                    dr[j - 1] = sheet.GetCellValueAsString(i, j);
+                }
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// Creates the data view.
+        /// </summary>
+        /// <param name="sheet">The sheet.</param>
+        /// <param name="hasHeaders">if set to <c>true</c> [has headers].</param>
+        /// <returns>DataView.</returns>
+        public static DataView CreateDataView(this SLDocument sheet, bool hasHeaders = true)
+        {
+            var dt = CreateDataTable(sheet, hasHeaders);
+            DataView dv = new DataView(dt);
+            return dv;
+        }
+    }
+}
